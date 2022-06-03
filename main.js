7136a65478749a076fca12d49e7c584490a38ad3
martingh19 <martigh14@gmail.com>
2022-06-03 11:06:25 -0500

    se agrego query parameters

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const API = 'https://api.thecatapi.com/v1/images/search';
+/**
+ * en esta url el endpoints es: /images/search o /images
+ */
+const API = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=0ece5eaa-08dd-412b-9389-476feb8956b6';
 
 const restAPI = async url_api =>
 {
     const answer = await fetch(url_api);
     const answerJSON = await answer.json();
+    console.log(answerJSON);
     imagePrincipal.src = answerJSON[0].url;
 }
 
